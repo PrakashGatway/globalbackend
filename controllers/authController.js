4fd0f1b01855c387e85cd78af87162bbaa0175aa
somesh <getwaytoabroadsomesh09@gmail.com>
2026-04-28 15:55:54 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ exports.login = async (req, res) => {
       })
     }
 
-    const otpCode = "987456" || Math.floor(100000 + Math.random() * 900000).toString()
+    const otpCode =  Math.floor(100000 + Math.random() * 900000).toString()
 
     await OTP.deleteMany({ email, isUsed: false })
 
@@ -40,7 +40,7 @@ exports.login = async (req, res) => {
       expiresAt: new Date(Date.now() + 5 * 60 * 1000), // 5 minutes
     })
     try {
-      // await sendOTPEmail({ email, otp: otpCode })
+      await sendOTPEmail({ email, otp: otpCode })
       res.json({
         success: true,
         isExist: true,
